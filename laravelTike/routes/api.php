3c8dad759c5468b7fb50f64c89b6b8eadfd5cfce
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-09-03 14:54:45 +0200

    Auth 1.0

--- laravelTike/routes/api.php
+++ laravelTike/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ShoesController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\BrandController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +22,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('shoes', ShoesController::class);
+Route::resource('types', TypeController::class);
+Route::resource('brands', BrandController::class);
+Route::resource('users', UserController::class);
+
+//Route::post('/register',[AuthController::class,'register']);
+//Route::post('/login',[AuthController::class,'login']);
+
+
+ Route::get('shoes/brand/{id}',[ShoesController::class,'getByBrand']);
+
+ Route::get('shoes/type/{id}',[ShoesController::class,'getByType']);
+
+
+//  Route::group(['middleware' => ['auth:sanctum']], function () {
+//     Route::get('/profile', function(Request $request) {
+//         return auth()->user();
+//     });
+
+// Route::get('my-shoes',[ShoesController::class,'myShoes']);
+
+//     Route::get('/logout',[AuthController::class,'logout']);
+
+//     Route::resource('skis',SkiController::class)->only('store','update','destroy'); 
+
+// });
